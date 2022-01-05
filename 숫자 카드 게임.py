75d4285bce61f52d5e85dc4a1c3f04f57c894574
HyeongGeun Kang <hgk5722@naver.com>
2022-01-05 18:14:00 +0900

    p.96

---
+++ 숫자 카드 게임.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    mini = min(data)
+    result = max(result, mini)
+
+print(result)
