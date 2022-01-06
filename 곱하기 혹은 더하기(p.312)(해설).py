f8fbb2f95f30e102cadb78b9226d6e96e8a14fae
HyeongGeun Kang <hgk5722@naver.com>
2022-01-06 12:51:43 +0900

    그리디

--- 곱하기 혹은 더하기(p.312)(해설).py
+++ 곱하기 혹은 더하기(p.312)(해설).py
@@ -1,7 +1,7 @@
 data = list(map(int, input()))
 
 data.sort()
-# 첫 리스트를 
+# 첫 리스트를 결과값으로 설정!!
 result = data[0]
 
 for i in range(1, len(data)):
@@ -12,5 +12,5 @@ for i in range(1, len(data)):
         result *= num
 print(result)
 
-# 처음 문제를 봤을때 0이 나오는 경우만 예외처리해주면 된다고 생각했었다.
-# 0, 1 모두 처리해 줘야함
+# 처음 생각 : 0만 예외처리, count()이용해서 0의 개수 파악하고 
+# zero변수 선언 후 data[zero]부터 리스트 끝까지 곱하면 된다고 생각함
