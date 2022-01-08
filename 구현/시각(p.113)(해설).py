dac1ee834ea535d9bd556eb569077f41287997c3
HyeongGeun Kang <hgk5722@naver.com>
2022-01-08 19:48:26 +0900

    구현

--- 구현/시각(p.113)(해설).py
+++ 구현/시각(p.113)(해설).py
@@ -1,10 +1,13 @@
 h = int(input())
 
 count = 0
+# 시간의 단위는 24시간 60분 60초
 for i in range(h + 1):
     for j in range(60):
         for k in range(60):
+            # 매시각에 문자열 '3'이 포함되어 있는지 확인
             if "3" in str(i) + str(j) + str(k):
+                # 반복 변수들을 문자열로 변환
                 count += 1
 print(count)
 
