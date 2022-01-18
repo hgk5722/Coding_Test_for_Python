2b216b27aa1c9a3d81319775faaa1e13899c0bf7
HyeongGeun Kang <hgk5722@naver.com>
2022-01-19 01:16:33 +0900

    bfs

--- DFS, BFS/인구 이동(p.353).py
+++ DFS, BFS/인구 이동(p.353).py
@@ -53,8 +53,8 @@ while True:
                 process(i, j, index)
                 index += 1 
     # 모든 인구 이동이 끝난 경우
-    if index == n * n:
-            break
+    if index == n * n: # 한번 더 돌아서 umion[x][y]의 요소에 index가 0부터 n-1까지 차례대로 들어갈때
+            break # index가 n이 되서 break 
     total_count += 1
 
 # 인구 이동 횟수 출력
