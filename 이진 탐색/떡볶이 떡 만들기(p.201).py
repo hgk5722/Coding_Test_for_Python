f4addf603242f57cb845574b9f3d89032705997c
HyeongGeun Kang <hgk5722@naver.com>
2022-01-27 16:52:53 +0900

    떡볶이 떡 만들기

--- 이진 탐색/떡볶이 떡 만들기(p.201).py
+++ 이진 탐색/떡볶이 떡 만들기(p.201).py
@@ -22,6 +22,7 @@ while start <= end:
         end = mid - 1 
     # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색) 
     else: 
+        # 적어도 m만큼은 떡을 얻어야 해서 else일때 절단기 길이를 저장, 그 후 반복으로 최적값 찾음 
         result = mid # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록 
         start = mid + 1 
 print(result) 
