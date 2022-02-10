6db85504cc867818ffb1cf04096ef6b4ec6e4b1c
HyeongGeun Kang <hgk5722@naver.com>
2022-02-10 19:45:18 +0900

    병사 배치하기

---
+++ 다이나믹 프로그래밍/병사 배치하기(p.380).py
@@ -0,0 +1,18 @@
+from re import X
+
+
+n = int(input()) 
+array = list(map(int, input().split())) 
+# 순서를 뒤집어 '가장 긴 증가하는 부분 수열' 문제로 변환 
+array.reverse()
+
+dp = [1] * n 
+# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
+for i in range(1, n):
+  for j in range(0, i):
+    if array[j] < array[i]:
+      dp[i] = max(dp[i], dp[j] + 1)
+
+# 최종 max(dp)가 가장 긴 증가하는 부분 수열 
+# 열외시켜야 하는 병사의 최소 수를 출력 
+print(n - max(dp)) 
