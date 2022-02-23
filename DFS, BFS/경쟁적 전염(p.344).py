913b6a1cf22cac5be2975e3bc8b375d86009f32d
HyeongGeun Kang <hgk5722@naver.com>
2022-02-23 22:43:27 +0900

    경쟁적 전염 Bfs

--- DFS, BFS/경쟁적 전염(p.344).py
+++ DFS, BFS/경쟁적 전염(p.344).py
@@ -14,7 +14,7 @@ for i in range(n):
             data.append((graph[i][j], 0, i, j)) # 튜플로 입력 
 
 data.sort()
-queue = deque(data) 
+queue = deque(data) # 탐색 시작 노드 큐에 넣기
 
 target_s, target_x, target_y = map(int, input().split()) 
 
@@ -22,7 +22,7 @@ dx = [ -1, 0, 1, 0 ]
 dy = [ 0, 1, 0, -1 ] 
 
 while queue:
-    virus, s, x, y = queue.popleft() 
+    virus, s, x, y = queue.popleft() # 큐에서 꺼내서 인접노드 확인 
     if target_s == s:
         break
     for i in range(4):
@@ -31,6 +31,6 @@ while queue:
         if 0 <= nx < n and 0 <= ny < n:
             # 아직 방문하지 않은 구역 
             if graph[nx][ny] == 0:
-                graph[nx][ny] = virus
-                queue.append((virus, s + 1, nx, ny)) 
+                graph[nx][ny] = virus # 인접한 노드 방문처리 
+                queue.append((virus, s + 1, nx, ny)) # 그후 큐에 넣기
 print(graph[target_x - 1][target_y - 1]) 
