970145e4ff9feb895580b80193a26949465b97c7
HyeongGeun Kang <hgk5722@naver.com>
2022-02-28 00:59:29 +0900

    국영수

--- 정렬/국영수(p.359).py
+++ 정렬/국영수(p.359).py
@@ -10,8 +10,8 @@ for i in range(n):
 4) 네 번째 원소가 같은 경우, 첫 번쨰 원소를 기준으로 오름차순 정렬
  """
 
-students.sort(key = lambda x : ( -int(x[1]), int(x[2]), -int(x[3]), x[0] )) 
+students.sort(key = lambda x : ( -int(x[1]), int(x[2]), -int(x[3]), x[0] )) # 튜플로 정렬
 
 # 정렬된 학생 정보에서 이름만 출력
 for student in students:
-    print(student[0]) 
+    print(student[0]) # 튜플로 정렬해서 특정 요소만 출력 가능 
