b2d8f74d6b15594867fc8366e69ec832720e84f3
HyeongGeun Kang <hgk5722@naver.com>
2022-03-16 01:32:03 +0900

    실패율

--- 정렬/실패율(p361).py
+++ 정렬/실패율(p361).py
@@ -2,7 +2,6 @@ def solution(N, stages):
     answer = []
     length = len(stages)
     
-    count = 0
     # 스테이지 번호를 1부터 N까지 증가시키며
     for number in range(1, N + 1):
         # 해당 스테이지에 머물러 있는 사람의 수 계산
@@ -12,14 +11,14 @@ def solution(N, stages):
         if length == 0:
             fail = 0
         else:
-            fail = count / length
+            fail = count / length # 정수 나누기 연산으로 하면 모두 0이 나오기 때문에
         
         # 리스트에 (스테이지 번호, 실패율) 원소 삽입
         answer.append((number, fail)) 
         length -= count 
     
     # 실패율을 기준으로 각 스테이지를 내림차순 정렬
-    answer.sort(key = lambda x : x[1], reverse=True ) 
+    answer.sort(key = lambda x : -x[1]) 
     
     answer = [ number[0] for number in answer ]
     return answer
