244941c121558f0753f9d1378a5806df313f3777
HyeongGeun Kang <hgk5722@naver.com>
2022-03-16 02:58:52 +0900

    카드 정렬하기

--- 정렬/카드 정렬하기(p.363).py
+++ 정렬/카드 정렬하기(p.363).py
@@ -6,12 +6,12 @@ n = int(input())
 heap = []
 for i in range(n): 
     data = int(input())
-    heapq.heappush(heap, data) 
+    heapq.heappush(heap, data) # heap리스트에 data를 넣는다.
 
 result = 0
 
 # 힙(heap)에 원소가 1개 남을 때까지
-while len(heap) != 1:
+while len(heap) != 1: # 마지막 sum값이 남으면 끝. 
     # 가장 작은 2개의 카드 묶음 꺼내기
     one = heapq.heappop(heap) 
     two = heapq.heappop(heap) 
