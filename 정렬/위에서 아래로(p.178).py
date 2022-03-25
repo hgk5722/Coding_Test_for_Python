acaca3c38363c0f491a7974f6ce47e17ec79a955
HyeongGeun Kang <hgk5722@naver.com>
2022-03-26 02:17:45 +0900

    위에서 아래로

--- 정렬/위에서 아래로(p.178).py
+++ 정렬/위에서 아래로(p.178).py
@@ -3,6 +3,6 @@ data = []
 for i in range(n):
     data.append(int(input())) 
 
-data.sort(reverse = True) 
+data.sort(reverse = True) # 역순으로 정렬
 for i in data:
-    print(i, end=' ') 
+    print(i, end=' ') # 공백을 사이에 두고 출력하는 방법
