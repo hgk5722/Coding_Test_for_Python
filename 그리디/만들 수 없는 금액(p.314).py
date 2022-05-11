3df3c9aac197376da8e56ae3cccab66c4ef99f66
HyeongGeun Kang <hgk5722@naver.com>
2022-05-11 14:37:36 +0900

    만들 수 없는 금액 p.314

--- 그리디/만들 수 없는 금액(p.314).py
+++ 그리디/만들 수 없는 금액(p.314).py
@@ -1,13 +1,30 @@
 n = int(input())
-data = list(map(int, input().split()))
-data.sort()
+coins = list(map(int, input().split()))
+coins.sort()
 
-target = 1
-for x in data:
+target = 1 # (target - 1) 까지는 모두 만들 수 있다고 가정
+for coin in coins:
     # 만들 수 없는 금액을 찾았을 때 반복 종료
-    if target < x:
+    if target < coin:
         break
-    target += x
+    target += coin
 
 print(target)
-# 어려운 그리디 문제 아이디어를 떠올리기 힘들다.
+""" 
+4
+1 2 3 8
+
+target = 1
+1 + 1 = 2
+
+2 + 2 = 4 = target
+
+3 = 1 + 2
+
+4 + 3 = 7 = target
+4
+2 + 3 = 5
+1 + 2 + 3 = 6
+
+7 + 8 = 15
+7은 만들 수 없다. """
