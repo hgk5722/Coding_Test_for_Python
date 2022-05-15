585bf0000580d30cc06bdd85a3d9642e11ff882c
HyeongGeun Kang <hgk5722@naver.com>
2022-05-15 23:54:07 +0900

    국영수

--- 정렬/국영수(p.359).py
+++ 정렬/국영수(p.359).py
@@ -2,7 +2,8 @@ import sys
 n = int(input()) 
 students = []
 for i in range(n):
-    students.append(sys.stdin.readline().split()) 
+    a, b, c, d = list(sys.stdin.readline().split())
+    students.append([a, b, c, d])
 
 """ [정렬 기준] 
 1) 두 번째 원소를 기준으로 내림차순 정렬
